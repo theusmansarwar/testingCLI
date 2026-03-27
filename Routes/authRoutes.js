1845ee1533f46ad035bd3a4720c58009afcc879c
theusmansarwar <themanbhai@gmail.com>
2026-03-27 16:19:55 +0300

    cli test 2

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -14,7 +14,7 @@ const authMiddleware = require("../Middleware/authMiddleware");
 // ===============================
 // AUTH ROUTES
 // ===============================
-router.post("/register-users", register);
+router.post("/register", register);
 router.post("/verify-email", verifyEmailOtp);
 router.post("/login", login);
 router.post("/forgot-password", forgotPassword);
